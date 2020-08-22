054306a13f2b67e46d73a67bd2b7422b31114134
Ong William Raven Wijaya <58552967+ravenwijaya@users.noreply.github.com>
2020-08-22 23:04:32 +0700

    Update About.js

--- final/src/pages/About.js
+++ final/src/pages/About.js
@@ -3,7 +3,7 @@ import React from "react"
 const About = () =>{
   return (
     <div style={{padding: "10px", border: "1px solid #ccc"}}>
-      <h1 style={{textAlign: "center"}}>Data Peserta Sanbercode Bootcamp Reactjs</h1>
+      <h1 style={{textAlign: "center"}}>Bootcamp Reactjs</h1>
       <ol>
         <li><strong style={{width: "100px"}}>Nama:</strong> nama peserta</li> 
         <li><strong style={{width: "100px"}}>Email:</strong> email pesera</li> 
